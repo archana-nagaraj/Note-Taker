dc2a207c3eef563cc058118936611abbc6613d16
Archana Nagaraja <johndoe@example.com>
2021-02-18 06:57:05 -0800

    storing Port no. in a variable and accessing in the listen function, code cleanup

--- express.js
+++ express.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const app = express();  // instantiate the server
+const PORT = 3002;
 
 //make the app listen to the server
 
-app.listen(3002, () => {
-    console.log('API server now listening to port 3002');
+app.listen(PORT, () => {
+    console.log(`API server now listening to port ${PORT}`);
 })
+
